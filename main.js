a6768dd882cfbdb4ff7ef4b5f67da2cd72d9296f
RoyElm <roye456@gmail.com>
2020-09-06 17:19:51 +0300

    version4

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function showSvg(task) {
     //BootStrap Element of svg to create X button
     const svg = `<svg width="22px" height="22px" viewBox="0 0 16 16" class="bi bi - x - svgClass square - fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" /></svg >`;
     //Creating a Div that will contain the svg
-    const divButton = document.createElement("a");
+    const divButton = document.createElement("div");
     divButton.innerHTML = svg;
     divButton.setAttribute("id", "button");
     divButton.setAttribute("onclick", "deleteTask()");
